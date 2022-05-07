6fdb86379affbdc9dce157ae87e8832849d5d05f
정재호 <b8739@hanmail.net>
2022-05-07 20:24:09 +0900

    프로그래머스 알고리즘 풀이

--- d.js
+++ d.js
@@ -1,45 +1,23 @@
-function solution(s) {
-  //최종 길이
-  var answer = 0;
-
-  //정답후보들, 아래 반복문에서 압축길이를 추출해서 넣어주고, 정답으로 이 중 최소를 꺼내게 됨
-  let lengthArr = [];
-
-  // S 길이까지 모든 압축 길이를 실험해봄
-  for (let i = 1; i <= s.length; i++)
-    lengthArr.push(compressedString(s, i).length);
-  console.log(lengthArr);
-  answer = Math.min(...lengthArr);
-
+function solution(places) {
+  var answer = [];
+  answer = places.map((place) => {
+    //전체 대기실
+    return place.some((room, rowIdx) => {
+      room.split("").some((mark, colIdx, roomArr) => {
+        if (mark === "X") return false;
+        let peopleAround = [
+          roomArr[colIdx - 1] || "",
+          roomArr[colIdx + 1] || "",
+          (place[rowIdx - 1] || "")[colIdx],
+          (place[rowIdx + 1] || "")[colIdx],
+        ].filter((mark) => mark == "P").length;
+        return (
+          (mark == "P" && peopleAround > 0) || (mark == "O" && peopleAround > 1)
+        );
+      });
+    })
+      ? 0
+      : 1;
+  });
   return answer;
 }
-
-//str은 압축할 문장이고 unitNum은 알파벳 몇개 단위로 압축할건지 (여기까지는 내 그리디 접근방식이 맞았음)
-function compressedString(str, unitNum) {
-  //몇개 단위로 압축되는지?
-  let count = 1;
-  //압축된 문자열
-  let result = [""];
-
-  for (let repeat = 0; repeat <= str.length / unitNum; repeat++) {
-    
-    const slicedGroup = str.slice(unitNum * repeat, unitNum * repeat + unitNum);
-
-    //똑같은 패턴으로 잘릴때마다 count를 늘림 (압축되는 과정)
-    if (result[result.length - 1] === slicedGroup) {
-      count++;
-    } 
-    
-    else {
-      if (count > 1)
-        result[result.length - 1] = count + result[result.length - 1];
-      result.push(slicedGroup);
-      console.log(result);
-      count = 1;
-    }
-  }
-
-  return result.join("");
-}
-let a = "ababcdcdababcdcd";
-solution(a);
